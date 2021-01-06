fa54ad6bfecf8b056b550294f7641de7fb1697c4
trevis yee <trevisyee@gmail.com>
2021-01-06 15:15:10 +1100

    add generation of two quotes

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,18 @@
-import random
+import random  #random is imported from built-in python module
 def centrala():
   # print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  f = open("quotes.txt")   #f is a variable defined as the contents of the quotes.txt
   quotes = f.readlines()
+  """quotes is a variable defined by the readlines() method done on the
+f variable or object as defined earlier"""
   f.close()
   
-  last = len(quotes) -1
+  last = len(quotes) -1 #len 
   rnd= random.randint(0, last)
-  
+  rnd2= random.randint(0, last)
   #print(quotes[-1])
-  print(quotes[rnd])
+  print(quotes[rnd]+"and "+quotes[rnd2])
   
 if __name__== "__main__":
   centrala()
